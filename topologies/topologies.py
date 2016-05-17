6d96394a8bae93f277df77552acef18216845900
bellockk <bellockk@segmail.com>
2016-05-17 09:09:52 -0400

    Separating out topology test

--- topologies/topologies.py
+++ topologies/topologies.py
@@ -35,19 +35,23 @@ class topology:
 
         # Test all possibilites and construct the list of actual topologies
 
-        a = self.topologies(masterlist, subsets)
+        a = self.find_topologies(masterlist, subsets)
 
         # Write the output file
         for item in a:
             LOG.info(str(item))
         LOG.info('\nTotal Topologies: %s' % str(len(a)))
 
-    def topologies(self, masterlist, subsets):
+    def append_topology(self, topology):
+        if topology:
+            self.topologies.append(topology)
+
+    def find_topologies(self, masterlist, subsets):
         """
         This algorithm tests each possible topology on the given basis and
         returns a list of all actual topologies.
         """
-        topologies = [subsets]
+        self.topologies = [subsets]
 
         # Apply the tests for a topology.  If it passes all tests add it to
         # topologies.
@@ -71,50 +75,54 @@ class topology:
                 bindex = bindex/2
             item.append(masterlist)
 
-            # Test for possesion of the empty set and the basis
-            test = 1
+            # Test if item is a topology
+            self.append_topology(test_topology(item))
+        return self.topologies
 
-            # Test that the intersection of any two members of the possible
-            # topology is in the possible topology.
-            if test:
-                itemsubsets = item
-                for i in xrange(len(itemsubsets)-1):
-                    if not test:
-                        break
-                    for j in xrange(i+1, len(itemsubsets) - 1):
-                        intersect = intersection(itemsubsets[i],
-                                                    itemsubsets[j])
-                        intersect.sort()
-                        if intersect not in item:
-                            test = 0
-                            break
 
-            # Test that the union of any family of subsets is contained in the
-            # possible topology.
-            if test:
-                nlength = 2**(len(item)-1)
-                while nlength > 0:
-                    nlength = nlength - 1
-                    nbindex = copy.deepcopy(nlength)
-                    family = []
-                    nsubindex = -1
-                    while nbindex >= 1:
-                        nsubindex = nsubindex + 1
-                        if nbindex % 2:
-                            family.append(item[nsubindex])
-                        nbindex = nbindex/2
-                    familyunion = union(family)
-                    familyunion.sort()
-                    if familyunion not in item:
-                        test = 0
-                        break
+def test_topology(item):
+    # Test for possesion of the empty set and the basis
+    test = 1
 
-            # If it passes all tests add the topology to the final list.
-            if test:
-                topologies.append(item)
+    # Test that the intersection of any two members of the possible
+    # topology is in the possible topology.
+    if test:
+        itemsubsets = item
+        for i in xrange(len(itemsubsets)-1):
+            if not test:
+                break
+            for j in xrange(i+1, len(itemsubsets) - 1):
+                intersect = intersection(itemsubsets[i],
+                                         itemsubsets[j])
+                intersect.sort()
+                if intersect not in item:
+                    test = 0
+                    break
 
-        # Return the resulting list of topologies
-        return topologies
+    # Test that the union of any family of subsets is contained in the
+    # possible topology.
+    if test:
+        nlength = 2**(len(item)-1)
+        while nlength > 0:
+            nlength = nlength - 1
+            nbindex = copy.deepcopy(nlength)
+            family = []
+            nsubindex = -1
+            while nbindex >= 1:
+                nsubindex = nsubindex + 1
+                if nbindex % 2:
+                    family.append(item[nsubindex])
+                nbindex = nbindex/2
+            familyunion = union(family)
+            familyunion.sort()
+            if familyunion not in item:
+                test = 0
+                break
+
+    # If it passes all tests add the topology to the final list.
+    if test:
+        return item
+    return None
 
 
 def allsubsets(masterlist):
@@ -129,6 +137,7 @@ def allsubsets(masterlist):
             alist.append(item)
             subsets.append(alist)
     return subsets
+
 
 def union(setofsets):
     """
@@ -145,12 +154,14 @@ def intersection(A, B):
     """
     return list(set(A).intersection(B))
 
+
 def compliment(subset, X):
     """
     This function returns the compliment of a set relative to the
     masterlist.
     """
     return [i for i in X if i not in subset]
+
 
 def permutations(masterlist):
     """
